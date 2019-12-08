f23e18b23ade6d4a2b20df49da349399afeee7a5
Perkylizard <pavanprabhavsinghtomar@gmail.com>
2019-12-08 23:38:48 +0530

    Updates

---
+++ precache-manifest.0cf5a43d176ca2cef06411bf5c1151cb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "876a38d408f3a08efc983cfe6db5490e",
+    "url": "/food/index.html"
+  },
+  {
+    "revision": "e3f2f83c921755e8d194",
+    "url": "/food/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "8cc6ece4b886a3517a90",
+    "url": "/food/static/css/main.852fc404.chunk.css"
+  },
+  {
+    "revision": "e3f2f83c921755e8d194",
+    "url": "/food/static/js/2.31232b45.chunk.js"
+  },
+  {
+    "revision": "8cc6ece4b886a3517a90",
+    "url": "/food/static/js/main.65225627.chunk.js"
+  },
+  {
+    "revision": "981f36cc8ef2bef31edb",
+    "url": "/food/static/js/runtime-main.03e07b6b.js"
+  },
+  {
+    "revision": "61fbdb11aa0079cd1cbf6bb59c709c35",
+    "url": "/food/static/media/Image.61fbdb11.png"
+  },
+  {
+    "revision": "17a572b8c520af4471f304cd3c9f2604",
+    "url": "/food/static/media/br.17a572b8.jpg"
+  },
+  {
+    "revision": "68bd3791be2d7c54d91f867a4bf59e2a",
+    "url": "/food/static/media/burger.68bd3791.jpg"
+  }
+]);
